2d32e1ffa3b55ab01db707ac8ba0561e4f9860b1
PedroMachadord <134148347+PedroMachadord@users.noreply.github.com>
2026-04-30 23:39:53 -0300

    Add files via upload

---
+++ hinos.js
@@ -0,0 +1,168 @@
+        const hymnsData = {
+            "1": { 
+                nome: "Cristo meu Mestre",
+                velMin: 56,
+                velMed: 61,
+                velMax: 66,
+                
+                versos: [
+                    "Cri|sto |meu |Mes|tre |e |meu |Se|nhor, |Eu |Te |a|do|ro, |por |fé, |com |fer|vor; |Ro|go |que |guar|des| meu |co|ra|ção;| Vem |pro|te|gê-|lo, |com| Tu|a  |un|ção |E |de|fen|dê-|lo, |ó |meu |Guar|di|ão.|",
+                    "Mes|tre |di|vi|no, |sem|pre |sen|ti |Meu |co|ra|ção |de|pen|den|te |de |Ti; |Bom |fun|da|men|to|dá-|lhe, |Se|nhor, |Dá-|lhe |fir|me|za, |vir|tu|de, |va|lor |E |for|ta|le|za, |ó |meu |Pro|te|tor.|",
+                    "Va|le |pro|fun|do, |chei|o |de |mal, |É |este| mun|do, |ó |Rei |di|vi|nal. |Só |Tu|a |for|ça| po|de |man|ter |San|to |e |pu|ro, |na| gra|ça, |meu |ser, |Sem|pre |se|gu|ro, |com |fé |e |po|der.|",
+                    "Mes|tre |pi|e |do|so, |com |Tu|a |mão, |Fa|ze |per|fei|to |o|meu |co|ra|ção, |San|to, |ar|do|ro|so |em |Te |ser|vir, |Sem|pre |vol|ta|do |aos |bens |do |por|vir; |Mes|tre |a|ma|do, |de|se|jo |Te |ou|vir.|"
+                ],
+                
+                tempos: [
+                    4, 2, 2, 4, 4, 4, 0, 2, 8, 4, 2, 2, 4, 2, 2, 4, 2, 2, 8, 4, 2, 2, 4,
+                    4, 4, 2, 2, 8, 4, 2, 2, 4, 2, 2, 4, 3, 1, 8, 4, 2, 2, 4, 18, 2, 4, 3, 1, 8
+                ],
+                
+                notas: [
+                    'G4', 'G4', 'G4', 'Bb4', 'Bb4', 'Eb4', 'Ab4', 'Ab4', 'G4', 'C5', 'C5', 'C5',
+                    'Bb4', 'G4', 'G4', 'G4', 'F4', 'F4', 'F4', 'G4', 'G4', 'Bb4', 'Bb4', 'Bb4',
+                    'C5', 'D5', 'Eb5', 'D5', 'Eb5', 'D5', 'C5', 'Bb4', 'G4', 'Bb4', 'Bb4', 'Bb4',
+                    'Ab4', 'G4', 'C5', 'D5', 'Eb5', 'Eb5', 'Eb4', 'F4', 'G4', 'G4', 'F4', 'Eb4'
+                ]
+            },
+
+            "2": { 
+                nome: "De Deus tu és eleita",
+                velMin: 60,
+                velMed: 70,
+                velMax: 80,
+                
+                versos: [
+                    "De| Deus| tu| és| e|lei|ta,| i|gre|ja| de| Je|sus,| O| teu| di|vi|no| Mes|tre| á| gló|ria| te| con|duz;| Pre|pa|ra|-te,| pois| E|le| mui| bre|ve| vol|ta|rá;| A| gló|ria| que| te| espe|ra| já| pre|pa|ra|da es|tá.|",
+                    "Com| ce|les|tiais| a|dor|nos| es|pe|ra o| teu| Se|nhor,| Ves|ti|da| de| jus|ti|ça| e| do| di|vi|no a|mor;| Di|le|ta| és| de| Cris|to,| que| vi|da| te| do|ou;| A| fim| de| res|ga|tar|-te,| Seu| san|gue| de|rra|mou.|",
+                    "É| gran|de| tu|a| gló|ria,| i|gre|ja| de| Je|sus;| Con|sa|gra|-te| a| E|le,| an|dan|do em| su|a| luz;| Em| bre|ve,| no| Seu| rei|no,| glo|rio|sa| en|tra|rás,| E a| fa|ce| do| E|spo|so,| no| céu,| con|tem|pla|rás.|"
+                ],
+                
+                tempos: [
+                    4, 4, 4, 4, 4, 8, 4, 
+                    4, 4, 4, 4, 4, 12, 
+                    4, 4, 4, 4, 4, 8, 4, 
+                    4, 4, 4, 4, 4, 12,
+                    4, 4, 4, 4, 4, 8, 4, 
+                    4, 6, 2, 4, 4, 12, 
+                    4, 4, 4, 4, 4, 8, 4, 
+                    4, 4, 4, 4, 4, 12,
+                    4, 4, 4, 4, 4, 8, 4, 
+                    4, 4, 4, 4, 4, 12, 
+                    4, 4, 4, 4, 4, 8, 4, 
+                    4, 4, 4, 4, 4, 12
+                ],
+                
+                notas: [
+                    'G4', 'G4', 'G4', 'Ab4', 'G4', 'G4', 'F4', 
+                    'Eb4', 'Eb4', 'C5', 'Bb4', 'Ab4', 'G4',
+                    'Ab4', 'Bb4', 'Eb5', 'Eb5', 'D5', 'D5', 'C5', 
+                    'Bb4', 'Ab4', 'Bb4', 'G4', 'Eb4', 'F4',
+                    'F4', 'G4', 'Ab4', 'Bb4', 'C5', 'C5', 'Bb4', 
+                    'Eb5', 'Eb5', 'D5', 'C5', 'G4', 'Ab4',
+                    'F4', 'G4', 'G4', 'Ab4', 'G4', 'G4', 'F4', 
+                    'Eb4', 'Eb4', 'F4', 'Eb4', 'D4', 'Eb4'
+                ]
+            },
+
+            "3": { 
+                nome: "Faz-nos ouvir Tua voz",
+                velMin: 60,
+                velMed: 68,
+                velMax: 76,
+                
+                versos: [
+                    "Faz|-nos| ou|vir| Tu|a| voz| glo|ri|o|sa,| Ó| nos|so| Mes|tre,| fi|el| Re|den|tor;| A| Tu|a i|gre|ja,| di|le|ta| es|po|sa,| Sen|te| por| Ti| i|ne|fá|vel| a|mor.|"+
+                    "\nÓ| ca|ro| Sal|va|dor,| a| Tu|a| voz| de a|mor| Nos| gui|a| pa|ra o| céu,| glo|rio|so| lar;| Pe|la| Pa|la|vra| vem| nos| con|so|lar.|",
+                    "Ó| Je|sus| Cris|to,| ce|les|te| Es|po|so,| Vem| con|du|zir|-nos,| por| Teu| gran|de a|mor;| Já| an|te|ve|mos,| ó| Rei| ma|jes|to|so,| A| gló|ria e|ter|na| do| Teu| es|plen|dor.|"+
+                    "\nÓ| ca|ro| Sal|va|dor,| a| Tu|a| voz| de a|mor| Nos| gui|a| pa|ra o| céu,| glo|rio|so| lar;| Pe|la| Pa|la|vra| vem| nos| con|so|lar.|",
+                    "A| Tu|a i|gre|ja| es|tá| se a|dor|nan|do| Pa|ra en|con|trar|-se| Con|ti|go, ó| Je|sus;| Com| san|tas| ves|tes,| es|tá| a|guar|dan|do| Ver| Tu|a| fa|ce| no| rei|no| de| luz.|"+
+                    "\nÓ| ca|ro| Sal|va|dor,| a| Tu|a| voz| de a|mor| Nos| gui|a| pa|ra o| céu,| glo|rio|so| lar;| Pe|la| Pa|la|vra| vem| nos| con|so|lar.|"
+                ],
+                
+                tempos: [
+                    4, 2, 2, 4, 2, 2, 4, 2, 2, 4, 4,
+                    4, 2, 2, 4, 2, 2, 4, 2, 2, 8,
+                    4, 2, 2, 4, 2, 2, 4, 2, 2, 4, 4,
+                    4, 2, 2, 4, 2, 2, 4, 2, 2, 8,
+
+                    4, 2, 2, 3, 1, 4, 4, 2, 2, 3, 1, 4,
+                    4, 2, 2, 2, 2, 4, 4, 2, 2, 8,
+                    4, 2, 2, 3, 1, 4, 4, 2, 2, 8   
+                ],
+                
+                notas: [
+                    'G4', 'G4', 'B4', 'G4', 'G4', 'G4', 'A4', 'A4', 'A4', 'B4', 'G4', 
+                    'C5', 'C5', 'C5', 'B4', 'B4', 'B4', 'A4', 'A4', 'A4', 'G4',      
+                    'G4', 'G4', 'B4', 'G4', 'G4', 'G4', 'A4', 'A4', 'A4', 'B4', 'G4', 
+                    'C5', 'C5', 'C5', 'B4', 'B4', 'B4', 'A4', 'A4', 'A4', 'G4',
+
+                    'A4', 'B4', 'A4', 'G4', 'B4', 'D5', 'A4', 'B4', 'A4', 'G4', 'B4', 'D5', 
+                    'C5', 'C5', 'C5', 'B4', 'G4', 'B4', 'A4', 'A4', 'A4', 'B4',
+                    'C5', 'C5', 'E5', 'B4', 'G4', 'B4', 'A4', 'A4', 'A4', 'G4'                       
+                ]
+            },
+
+            "4": { 
+                nome: "Ouve a nossa oração",
+                velMin: 112,
+                velMed: 125,    
+                velMax: 138,
+                
+                versos: [
+                    "Nos|sos| o|lhos| pa|ra os| céus, |"+
+                    "ó| Deus,| nos| le|van|ta|mos; |"+ 
+                    "Con|fi|an|do em| Teu| a|mor, |"+
+                    "a| nos|sa| voz| al|ça|mos.|"+
+
+                    "\nOu|ve a| nos|sa| o|ra|ção |"+
+                    "Des|de a| Tu|a ha|bi|ta|ção; |"+ 
+                    "Tu|a| ben|ção,| Teu| per|dão, |"+ 
+                    "Man|da, ó Deus| pi|e|do|so.|", 
+                    
+                    "Re|fri|gé|rio| ce|les|tial |"+
+                    "e |bál|sa|mo| pre|cio|so |"+ 
+                    "É| sen|tir|-Te em| nos|so| ser, |"+
+                    "Pai| san|to,| glo|ri|o|so.|"+
+
+                    "\nOu|ve a| nos|sa| o|ra|ção |"+
+                    "Des|de a| Tu|a ha|bi|ta|ção; |"+ 
+                    "Tu|a| ben|ção,| Teu| per|dão, |"+ 
+                    "Man|da, ó Deus| pi|e|do|so.|", 
+
+                    "No| Teu| tro|no,| ó| Se|nhor, |"+
+                    "re|ce|be| nes|ta| ho|ra |"+ 
+                    "A| hu|mil|de| o|ra|ção |"+
+                    "do| po|vo| que| Te a|do|ra.|"+
+
+                    "\nOu|ve a| nos|sa| o|ra|ção |"+
+                    "Des|de a| Tu|a ha|bi|ta|ção; |"+ 
+                    "Tu|a| ben|ção,| Teu| per|dão, |"+ 
+                    "Man|da, ó Deus| pi|e|do|so.|" 
+                ],
+                
+                tempos: [
+                    4, 2, 4, 2, 4, 2, 4,
+                    2, 4, 2, 4, 2, 6, 6,
+                    
+                    4, 2, 4, 2, 4, 2, 4,
+                    2, 4, 2, 4, 2, 6, 6,
+
+                    4, 2, 4, 2, 4, 2, 6,
+                    4, 2, 4, 2, 4, 2, 6,
+
+                    4, 2, 4, 2, 4, 2, 6,
+                    4, 2, 4, 2, 6, 6
+                ],
+                
+                notas: [
+                    'C5', 'C5', 'B4', 'B4', 'C5', 'C5', 'Bb4',
+                    'Ab4', 'Eb4', 'Ab4', 'Bb4', 'Ab4', 'Ab4', 'G4',
+                    'Db5', 'Db5', 'C5', 'C5', 'Db5', 'Bb4', 'G4',
+                    'Eb4', 'Ab4', 'Bb4', 'C5', 'D5', 'Eb5', 'Eb4',
+                    'C5', 'C5', 'Db5', 'C5', 'C5', 'Bb4', 'F4',
+                    'Bb4', 'Bb4', 'C5', 'Bb4', 'Bb4', 'Ab4', 'Eb4',
+                    'C4', 'Eb4', 'Ab4', 'Bb4', 'C5', 'Eb5', 'Db5',
+                    'C5', 'Ab4', 'C5', 'Ab4', 'Bb4', 'Ab4',
+                ]
+            },
+        };
